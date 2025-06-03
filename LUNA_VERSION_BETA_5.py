7e79dfa90d963d340ca962394f64391baad7d5c3
Samar <samarjyotipator78@gmail.com>
2025-06-03 20:02:03 +0530

    Add files via upload

---
+++ LUNA_VERSION_BETA_5.py
@@ -0,0 +1,551 @@
+import os
+import random
+print("A DATE WITH LUNA")
+def mngend_0():
+    try:
+        with open("ending_0.txt","r") as arpit:
+            a = arpit.read()
+            if a != "completed":
+                with open("ending_0.txt","w") as arpit:
+                    arpit.write("incomplete") 
+            else:
+                pass
+    except FileNotFoundError:
+        with open("ending_0.txt","w") as arpit:
+            arpit.write("incomplete")  
+def mngend_1():
+    try:
+        with open("ending_1.txt","r") as arpit:
+            a = arpit.read()
+            if a != "completed":
+                with open("ending_1.txt","w") as arpit:
+                    arpit.write("incomplete") 
+            else:
+                pass
+    except FileNotFoundError:
+        with open("ending_1.txt","w") as arpit:
+            arpit.write("incomplete")  
+def mngend_2():
+    try:
+        with open("ending_2.txt","r") as arpit:
+            a = arpit.read()
+            if a != "completed":
+                with open("ending_2.txt","w") as arpit:
+                    arpit.write("incomplete") 
+            else:
+                pass
+    except FileNotFoundError:
+        with open("ending_2.txt","w") as arpit:
+            arpit.write("incomplete")         
+def mngend_3():
+    try:
+        with open("ending_3.txt","r") as arpit:
+            a = arpit.read()
+            if a != "completed":
+                with open("ending_3.txt","w") as arpit:
+                    arpit.write("incomplete") 
+            else:
+                pass
+    except FileNotFoundError:
+        with open("ending_3.txt","w") as arpit:
+            arpit.write("incomplete")
+def mngend_4():
+    try:
+        with open("ending_4.txt","r") as arpit:
+            a = arpit.read()
+            if a != "completed":
+                with open("ending_4.txt","w") as arpit:
+                    arpit.write("incomplete") 
+            else:
+                pass
+    except FileNotFoundError:
+        with open("ending_4.txt","w") as arpit:
+            arpit.write("incomplete") 
+def mngend_5():
+    try:
+        with open("ending_5.txt","r") as arpit:
+            a = arpit.read()
+            if a != "completed":
+                with open("ending_5.txt","w") as arpit:
+                    arpit.write("incomplete") 
+            else:
+                pass
+    except FileNotFoundError:
+        with open("ending_5.txt","w") as arpit:
+            arpit.write("incomplete")            
+def mngend_6():
+    try:
+        with open("ending_6.txt","r") as arpit:
+            a = arpit.read()
+            if a != "completed":
+                with open("ending_6.txt","w") as arpit:
+                    arpit.write("incomplete") 
+            else:
+                pass
+    except FileNotFoundError:
+        with open("ending_6.txt","w") as arpit:
+            arpit.write("incomplete") 
+def mngend_7():
+    try:
+        with open("ending_7.txt","r") as arpit:
+            a = arpit.read()
+            if a != "completed":
+                with open("ending_7.txt","w") as arpit:
+                    arpit.write("incomplete") 
+            else:
+                pass
+    except FileNotFoundError:
+        with open("ending_7.txt","w") as arpit:
+            arpit.write("incomplete")            
+def mngend_8():
+    try:
+        with open("ending_8.txt","r") as arpit:
+            a = arpit.read()
+            if a != "completed":
+                with open("ending_8.txt","w") as arpit:
+                    arpit.write("incomplete") 
+            else:
+                pass
+    except FileNotFoundError:
+        with open("ending_8.txt","w") as arpit:
+            arpit.write("incomplete")  
+def mngend_9():
+    try:
+        with open("ending_9.txt","r") as arpit:
+            a = arpit.read()
+            if a != "completed":
+                with open("ending_9.txt","w") as arpit:
+                    arpit.write("incomplete") 
+            else:
+                pass
+    except FileNotFoundError:
+        with open("ending_9.txt","w") as arpit:
+            arpit.write("incomplete")      
+def mngend_10():
+    try:
+        with open("ending_10.txt","r") as arpit:
+            a = arpit.read()
+            if a != "completed":
+                with open("ending_10.txt","w") as arpit:
+                    arpit.write("incomplete") 
+            else:
+                pass
+    except FileNotFoundError:
+        with open("ending_10.txt","w") as arpit:
+            arpit.write("incomplete")  
+def mngend_11():
+    try:
+        with open("ending_11.txt","r") as arpit:
+            a = arpit.read()
+            if a != "completed":
+                with open("ending_11.txt","w") as arpit:
+                    arpit.write("incomplete") 
+            else:
+                pass
+    except FileNotFoundError:
+        with open("ending_11.txt","w") as arpit:
+            arpit.write("incomplete")  
+def mngend():
+    mngend_0()
+    mngend_1()
+    mngend_2()
+    mngend_3()
+    mngend_4()  
+    mngend_5()     
+    mngend_6()
+    mngend_7()              
+    mngend_8()    
+    mngend_9()
+    mngend_10()
+    mngend_11()
+def end():
+    with open("ending_0.txt","r") as python:
+        py = python.read()
+    with open("ending_1.txt","r") as asmodeus:
+        a = asmodeus.read()
+    with open("ending_2.txt","r") as beelzebub:
+        b = beelzebub.read()
+    with open("ending_3.txt","r") as colgate:
+        c = colgate.read()
+    with open("ending_4.txt","r") as demon:
+        d = demon.read()
+    with open("ending_5.txt","r") as elf:
+        e = elf.read()    
+    with open("ending_6.txt","r") as focalors:
+        f = focalors.read()               
+    with open("ending_7.txt","r") as god:
+        g = god.read()  
+    with open("ending_8.txt","r") as hell:
+        h = hell.read()  
+    print(f"ending 0-{py}")         
+    print(f"ending 1-{a}") 
+    print(f"ending 2-{b}")     
+    print(f"ending 3-{c}")  
+    print(f"ending 4-{d}")  
+    print(f"ending 5-{e}") 
+    print(f"ending 6-{f}") 
+    print(f"ending 7-{g}")
+    print(f"ending 8-{h}")
+def ending_0():
+    with open("ending_0.txt","w") as f:
+        f.write("completed")      
+def ending_1():
+    with open("ending_1.txt","w") as f:
+        f.write("completed")    
+def ending_2():
+    with open("ending_2.txt","w") as f:
+        f.write("completed")
+def ending_3():
+    with open("ending_3.txt","w") as f:
+        f.write("completed")
+def ending_4():
+    with open("ending_4.txt","w") as f:
+        f.write("completed") 
+def ending_5():
+    with open("ending_5.txt","w") as f:
+        f.write("completed")  
+def ending_6():
+    with open("ending_6.txt","w") as f:
+        f.write("completed")    
+def ending_7():
+    with open("ending_7.txt","w") as f:
+        f.write("completed")    
+def ending_8():
+    with open("ending_8.txt","w") as f:
+        f.write("completed")  
+def ending_9():
+    with open("ending_9.txt","w") as f:
+        f.write("completed")    
+def ending_10():
+    with open("ending_10.txt","w") as f:
+        f.write("completed")      
+def ending_11():
+    with open("ending_11.txt","w") as f:
+        f.write("completed")      
+def gen_pass():
+    if password() == None:
+        b = ""
+        for a in range(0,6):
+            c = random.randint(0,9)
+            d = str(c)
+            b += d
+        with open("ADWLGLP.txt","w") as f: 
+            f.write(b)
+    else:
+        b = password()
+        with open("ADWLGLP.txt","w") as f: 
+            f.write(b)    
+def password():
+    try:
+        with open("ADWLGLP.txt","r") as f:
+            a = f.read()
+    except FileNotFoundError:
+        a = None
+    return a           
+def name():
+    try:
+        with open("ADWLGUN.txt", "r") as file:
+            return file.read()
+    except FileNotFoundError:
+        return None
+def invalid():
+    print("invalid input") 
+    if name() == None:
+        print("try again!") 
+    else:
+        print(f"try again, {name()}!") 
+def scn02():
+    print('"what? you were the one who wasn'+"'"+'walking properly, get yourself new eyes"')
+    print("you both get into a argument")
+    print("you both return home")
+    print("ENDING 0-THE WHY ARE YOU EVEN PLAYING IT ENDING")
+    ending_0()
+    end()
+def scn012():
+    print("Error 404")
+    print("visit my github accounts at https://github.com/fatal-error-404-samar")
+    print("I'm not implenting it soon")
+    print("Have a good day")
+    print("#content will be added later")
+def scn01122():
+    print('"WHAT THE HELL? F🤬CK OFF! I'+"'"+'M LEAVING"')
+    print("they left the house")
+    print("ENDING 11-THE MISSION FAILED ENDING")
+    ending_11()
+    end()
+def scn01121():    
+    print('"yea sure"')
+    print("you both play games for quite some times")
+    print('you say,"WOAH! that was fun!"')
+    print('"agreed"')
+    print("options:- ")
+    print('1. "can I have your number?"')
+    print('2. "wanna have Netflix and chill?"')
+    Arpit == input("enter your response: ")
+    if Arpit == "1":
+        print('"yeah sure. why not?"')
+        print("ENDING 9-THE MAYBE YOU HAVE A CHANCE ENDING")
+        ending_9()
+        end()
+    elif Arpit == "2":
+        print('"just today, I guess"')
+        print("ENDING 10-THE HOOK-UP ENDING")
+        ending_10()
+        end()
+    else:
+        invalid()
+        scn01121()
+def scn0112():
+    print('"okay"')
+    print("you both go to your house")
+    print('you say,"feel yourself at home".')
+    print("you go to your kitchen and come back with 2 icecreams")
+    print("you give one icecream to luna")
+    print("you both eat your icecreams")
+    print("options:-")
+    print('1. "wanna play some games?"')
+    print('2. "wanna have Netflix and chill"')
+    Arpit= input("Enter your response: ")
+    if Arpit == "1":
+        scn01121()
+    elif Arpit == "2":
+        scn01122()
+    else:
+        invalid()
+        scn0112()
+def scn01112():
+    print("WHAT THE FUCK ? why'd you bring me here?")
+    print('"to eat", you answer.")
+    print('The waiter asks,"so you two don'+"'"+'t have money?"')
+    print('"NOPE", you both answer.')
+    print("THE WAITER SAYS:-")
+    print('"ATLEAST YOU TWO CAN BE SOLD"')
+    print("suddenly, you two became unconscious!")
+    print("ENDING 8-THE SLAVERY ENDING")
+    ending_8()
+    end()
+def scn011112():
+    print("Oh! no problem! wanna be friends then?")
+    print("options:-")
+    print('1. "yes"')
+    print('2. "no"')
+    arpit = input("enter your response: ")
+    if arpit == "1":
+        print("ENDING 6-THE FRIENDS ENDING")
+        ending_6()
+        end()
+    elif arpit == "2":
+        print("ENDING 7-THE LONER ENDING")
+        ending_7()
+        end()
+    else:
+        invalid()
+        scn011112()    
+def scn01111121():
+    print("IT UNLOCKED!!!")
+    print("but you notice something strange")
+    print("The wallpaper is animated. But that's not what is strange. strange thing is that the wallpaper is of Luna butchering you!")
+    print("You were scared, and you tried to run")
+    print("but, poor you. Luna returns, with a axe in hand!")
+    print("Ending 4-THE BUTCHER ENDING")
+    ending_4()
+    end()
+def scn01111122():
+    print("WELL, A INVALID PASSWORD WON'T BE LEFT UNPUNISHED")
+    print("Luna comes, with a axe in her hands. you beg for mercy. But, you get tortured and slaughtered anyway.")
+    print("ENDING 5-THE PUNISHMENT ENDING")
+    ending_5()
+    end()
+def scn0111112():
+    print("You try to open it")
+    print("it has a 6-digit Password")
+    print("guess it")
+    guess = input("enter the password: ")
+    gen_pass()
+    if guess == password():
+        scn01111121()
+    else:
+        print("Invalid password!")
+        scn01111122()    
+def scn01111112():
+    print("So, can I have your number then?")
+    print("options:-")
+    print('1. "Yes"')
+    print('2. "No"')
+    Arpit = input("Enter your response: ")
+    if Arpit == "1":
+        print("Ending 1-THE NON-FREAKY DATE ENDING!")
+        ending_1()
+        end()
+    elif Arpit == "2":
+        print("Ending 3-THE LOST CHANCE ENDING!")
+        ending_3()
+        end()
+    else:
+        invalid()
+        scn01111112()    
+def scn0111111():
+    print("You continue to play games as usual.")
+    print('She returns and says,"Let'+"'"+'s do something else"')
+    print('You say,"Okay"')
+    print('She says,"wanna have Netflix and chill?"')
+    print("Options:-")
+    print('1. "HELL YEAH!"')
+    print('2. "HELL NO!"')
+    Arpit = input("Enter your response: ")
+    if Arpit == "1":
+        print("Ending 2-THE FRIENDS WITH BENEFITS ENDING!")
+        ending_2()
+        end()
+    elif Arpit == "2":
+        scn01111112()
+    else:
+        invalid()
+        scn0111111()    
+def scn011111():
+    print('"Yippee! Here, follow me. my house is nearby."')
+    print("You both went to their apartment.")
+    print('"You both enter her apartment and she say,"it'+"'"+'s a little bit messy. But, feel yourself at home!"')
+    print("she brings her gaming console and you both start playing games.")
+    print('Suddenly she gets up and says,"I have to go to the washroom. I'+"'"+'right back."')
+    print('You say,"Okay"')
+    print("You suddenly notice her phone")
+    print("Do you want to try unlocking it?")
+    print("options:-")
+    print("1. No")
+    print("2. Yes")
+    arpit = input("Enter your response: ")
+    if arpit == "1":
+        scn0111111()
+    elif arpit == "2":
+        scn0111112()
+    else:
+        invalid()
+        scn011111()       
+def scn01111():
+    print('"THANKS MAN!"')
+    print("You pay the bill.")
+    print("You two leave the restaurant.")
+    print(f'"Hey {name()}! Let'+"'"+'s go to my house. There are a lot of games there."')
+    print("options:-")
+    print('1. "Okay."')
+    print('2. "Sorry, I have to go."')
+    e = input("Enter your response: ")
+    if e == "1":
+        scn011111()
+    elif e == "2":
+        scn011112()
+    else:
+        invalid()
+        scn01111()    
+def scn0111():
+    print('"THANKS! There is a 5-star restaurant near us. Let'+"'s go there."+'"')
+    print("You 2 go to the restaurant.")
+    print('"THANKS AGAIN! Let'+"'s see what's on the menu."+"'")
+    print("It says:-")
+    print("1. Hamburgers.")
+    print("2. Veg-burgers.")
+    print("3. French fries.")
+    print("4. Pizzas.")
+    print("5. Chicken nuggets.")
+    d = input("let's eat ")
+    e = ""
+    if d == "1":
+        e = "Hamburgers"
+    elif d == "2":
+        e = "Veg-burgers"
+    elif d == "3":
+        e = "French fries"
+    elif d == "4":
+        e = "Pizzas"
+    elif d == "5":
+        e = "Chicken nuggets"
+    else:
+        invalid()
+        scn0111()
+    print(f'"{e} are my favourite!"')          
+    print(f"they call the waiter and give an order of two {e}.") 
+    print("after that, the waiter comes with the bill.")
+    print("the total bill is $20")
+    print("options:-")
+    print('1. "Okay, I'+"'"+'ll pay."')
+    print('2. "I don'+"'"+'t have money."')
+    e = input("Enter your response: ")
+    if e == "1":
+        scn01111()
+    elif e == "2":
+        scn01112()
+    else:
+        invalid()
+        scn0111()        
+def scn011():
+    print('"THANKS! But, there is no Icecream seller open right now."')
+    print('1. "Let'+"'s"+' go to a restaurant then."')
+    print('2. "Let'+"'s"+' go to my house. There is some Icecream in my fridge."')
+    d = input("Enter your response: ")
+    if d == "1":
+        scn0111()
+    elif d == "2":
+        scn0112()
+    else:
+        invalid()
+        scn011()    
+def scn01():
+    print('"oh! no problem! What'+"'s"+' your name?"')
+    if name() == None:
+        c = input("Enter your name: ")
+        with open("ADWLGUN.txt", "w") as file:
+            file.write(c)       
+    else:
+        print("Do you want to change your name?(Y/N)")
+        samar = input().upper()
+        if samar == "Y":
+            c = input("Enter your name: ")
+            with open("ADWLGUN.txt", "w") as file:
+                file.write(c)  
+        elif samar == "N":
+            pass
+        else:
+            invalid()    
+            scn01()    
+    print(f"{name()}? that's a nice name! by the way, I'm Luna. It's nice to meet you, {name()}!") 
+    print("looks like my clothes got dirty by the Icecream I was eating.")   
+    print("1. I'll buy you a new Icecream.")
+    print("2. I'll buy you some new clothes.")
+    d = input("Enter your response: ")
+    if d == "1":
+        scn011()
+    elif d == "2":
+        scn012()
+    else:
+        invalid()
+        scn01()    
+def scn0():
+    b = input("Enter your response: ")
+    if b == "1":
+        scn01()
+    elif b == "2":
+        scn02()
+    else:
+        invalid()
+        scn0()
+def game():
+    print("you are going your home. when, suddenly! you crash into someone.")
+    print("Options:-")
+    print("1. say sorry")
+    print("2. accuse them of not walking properly.")
+    scn0()
+def main():
+    a = True    
+    while a:
+        mngend()    
+        print("do you want to start?(Y/N)")
+        b = input("enter your response: ").upper()
+        if b == "Y":
+            game()
+        elif b == "N":
+            print("Game ended")
+            a = False
+        else:
+            invalid()
+main()            
